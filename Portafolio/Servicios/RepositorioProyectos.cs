f08103c03ceddc92d2c43f341a850d692c42f45b
Jc3046 <jeanalcazar26@gmail.com>
2024-06-02 22:21:37 -0700

    Principio Responsabilidad Unica

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,44 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDIO> ObtenerProyectos()
+        {
+            return new List<ProyectoDIO>() {
+                new ProyectoDIO
+            {
+                Titulo="Amazon",
+                Descripcion="E-Commerce realziado en ASP.NET Core",
+                Link="https://amazon.com",
+                ImagenURL="/imagenes/amazon.png"
+            },
+
+             new ProyectoDIO
+            {
+                Titulo="New York Times",
+                Descripcion="Pagina de noticias en React",
+                Link="https://nytimes.com",
+                ImagenURL="/imagenes/NYT.png"
+            },
+
+              new ProyectoDIO
+            {
+                Titulo="Redit",
+                Descripcion="Red social para compartir en comunidades",
+                Link="https://reddit.com",
+                ImagenURL="/imagenes/reddit.png"
+            },
+
+             new ProyectoDIO
+            {
+                Titulo="Steam",
+                Descripcion="Tienda en linea para comprar videojuegos",
+                Link="https://store.steampowered.com",
+                ImagenURL="/imagenes/steam.png"
+            },
+            };
+        }
+    }
+}
