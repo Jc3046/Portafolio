5b24127f3ef1d64d98613456f4a7ae81a4dac813
Jc3046 <jeanalcazar26@gmail.com>
2024-06-03 18:46:44 -0700

    Creando un simple formulario

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -30,7 +30,12 @@ namespace Portafolio.Controllers
             return View(proyectos); 
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Contacto()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
